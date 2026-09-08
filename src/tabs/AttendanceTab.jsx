ba816ecd8790db9fc5bb59dc62449935bca50be1
feezoapp <feezosolution@gmail.com>
2026-09-08 13:10:38 +0530

    Add files via upload

--- src/tabs/AttendanceTab.jsx
+++ src/tabs/AttendanceTab.jsx
@@ -461,7 +461,7 @@ export default function AttendanceTab() {
         setLoading(false);
       }
     })();
-  }, [academyId, date, viewMode, year, month, reloadKey, students, visibleSports]);
+  }, [academyId, date, viewMode, year, month, reloadKey, students, visibleSports, sportFilter, batchFilter]);
 
   // ---- Realtime sync ----
   // AcademyDataContext already keeps sports/batches/students/enrollments live;
